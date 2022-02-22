0873e103fc213c7083902e8813e5ec3d2a27dae3
integral <volkovanton71@yandex.ru>
2022-02-22 13:48:39 +0300

    ts_3_task

--- ts_3_task/src/index.ts
+++ ts_3_task/src/index.ts
@@ -1,2 +1,10 @@
-// import axios from "axios";
+import {getTodosByCount} from "./todos.js";
+
+getTodosByCount(3)
+  .then((todos) => {
+    console.log('todos', todos)
+  })
+  .catch((error) => {
+    console.error(error)
+  });
 
